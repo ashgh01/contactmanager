144b1ddbf10e8d8cda3eab88c1326989e0c7a270
ashgh01 <ashokghosh@live.com>
2017-10-25 10:26:57 -0700

    Update DisableRemote.ps1

--- DisableRemote.ps1
+++ DisableRemote.ps1
@@ -1,3 +1,3 @@
 ﻿
 
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 1
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 1
